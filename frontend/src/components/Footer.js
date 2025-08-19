5381ce52639ab71806abe91752d694a85a001776
saumin <bhagatwalasaumin283@gmail.com>
2025-08-18 18:25:13 -0600

    Fix the footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
   return (
     <footer className="app-footer">
-      <p>&copy; {currentYear} My Friend's Podcast. All Rights Reserved.</p>
+      <p>&copy; {currentYear} A Little Perspective. All Rights Reserved.</p>
     </footer>
   );
 };
